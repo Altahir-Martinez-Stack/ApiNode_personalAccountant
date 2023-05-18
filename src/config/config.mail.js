311dba4b8f5a831495b8725112b6f96504e28b1e
Altahir <altahir_mp@outlook.com>
2023-05-17 20:52:41 -0500

    src/config/config.mail.js <-- la funcion de recipientsEmail se movio

--- src/config/config.mail.js
+++ src/config/config.mail.js
@@ -1,13 +1,35 @@
 const nodemailer = require("nodemailer");
 
+//cuenta de correo principal
+let mail = {
+  user: "testpruebasperu01@gmail.com",
+  password: "rlijsgpqnotgcmki",
+  type: "smtp.gmail.com",
+};
+
 // create reusable transporter object using the default SMTP transport
 const config = {
-  host: "smtp.gmail.com",
+  host: mail.type,
   port: 587,
   auth: {
-    user: "testpruebasperu01@gmail.com", // generated ethereal user
-    pass: "rlijsgpqnotgcmki", // generated ethereal password
+    user: mail.user, // generated ethereal user
+    pass: mail.password, // generated ethereal password
   },
 };
 
-export const transport = nodemailer.createTransport(config);
+//tranport es que envia el correo
+const transport = nodemailer.createTransport(config);
+
+// send mail with defined transport object
+function recipientsEmail(mailAddresses, subjectAddressee, descriptionMessage) {
+  const message = {
+    from: mail.user, // sender address
+    to: mailAddresses, // list of receivers
+    subject: subjectAddressee, // Subject line
+    text: descriptionMessage, // plain text body
+    //   html: "<b>Hello world?</b>", // html body
+  };
+  return message;
+}
+
+module.exports = { transport, recipientsEmail };
--- src/controllers/mail.controller.js
+++ src/controllers/mail.controller.js
@@ -1,27 +1,18 @@
-import { transport } from "../config/config.mail.js";
+import { transport, recipientsEmail } from "../config/config.mail.js";
 
-// send mail with defined transport object
-function recipientsEmail(mail, subjecttxt, description) {
-  const message = {
-    from: "testpruebasperu01@gmail.com", // sender address
-    to: mail, // list of receivers
-    subject: subjecttxt, // Subject line
-    text: description, // plain text body
-    //   html: "<b>Hello world?</b>", // html body
-  };
-  return message;
-}
-
-//
+//para enviar mensaje
 export const sendMailTo = async (req, res) => {
+  //Datos que se envias desde el front
   const { mail, subject, description } = req.body;
 
+  //valida si no es null
   if (mail == null || subject == null || description == null) {
     return res.status(400).json({ msg: "Bad Request. Please Fill all fields" });
   }
   try {
     const sendMessage = recipientsEmail(mail, subject, description);
     await transport.sendMail(sendMessage);
+
     res.send("confirmed request: The mail was sent");
   } catch (error) {
     return res.status(500).json({ message: error.message });
