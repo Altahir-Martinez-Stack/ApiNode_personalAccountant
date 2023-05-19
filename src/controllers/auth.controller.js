b3fc5396cc59276e92fb78809a0007d2b82ecaed
Altahir <altahir_mp@outlook.com>
2023-05-18 23:02:36 -0500

    configurando el token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,12 @@
 //models
 import { User } from "../models/Users";
 import { encrypt, compare } from "../helpers/HandleBcrypt.js";
-import { token } from "morgan";
+const jwt = require("jsonwebtoken");
+
+import { config as dotenv } from "dotenv";
+dotenv();
+
+const secret = process.env.SECRET;
 const { Op } = require("sequelize");
 
 //crear un nuevo registro en la tabla user
@@ -17,6 +22,18 @@ export const registerLogin = async (req, res) => {
   //encrytando la contraseña
   const passwordHash = await encrypt(password);
   try {
+    const validateUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    //Valida si existe el id
+    if (validateUser) {
+      return res
+        .status(400)
+        .json({ msg: "Bad Request. that email is already registered" });
+    }
     //creando los datos en la tabla de users
     const newUser = await User.create({
       email,
@@ -46,19 +63,69 @@ export const login = async (req, res) => {
         msg: "Bad Request. error does not exist the username and password",
       });
     }
-    console.log("sdasa:", user);
     //Comprar la contraseña con la conttraseña encrytada
     const checkPassword = await compare(password, user.password);
     if (checkPassword) {
-      res.send("request accepted, user is correct");
       // res.send({
       //   data: user.name,
       // });
+      var name = user.name;
+      const token = jwt.sign(
+        {
+          name,
+          exp: Date.now() + 60 * 1000,
+        },
+        secret
+      );
+      console.log({ token });
+      res.send("request accepted, user is correct");
     } else {
       return res.status(404).json({
         msg: "Bad Request. error does not exist the username and password",
       });
     }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const token = async (req, res) => {
+  const { id: sub, name } = { id: 4, name: "renzo" };
+  const token = jwt.sign(
+    {
+      sub,
+      name,
+      exp: Date.now() + 60 * 1000,
+    },
+    secret
+  );
+  res.send({ token });
+  try {
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const publics = async (req, res) => {
+  res.send("I'm public");
+  try {
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const privates = async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    throw new Error("Authentication failed!");
+  }
+
+  try {
+    const payload = jwt.verify(token, secret);
+    if (Date.now() > payload.exp) {
+      return res.status(401).send({ error: "token expired" });
+    }
+    res.send("I'm private");
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { registerLogin, login } from "../controllers/auth.controller";
+import {
+  registerLogin,
+  login,
+  token,
+  privates,
+} from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post("/auth", login);
 router.post("/auth/register", registerLogin);
+router.post("/auth/token", token);
+router.get("/auth/private", privates);
 
 export default router;
